64311a69fc5a71d7f756056b9aef0dc6e7c29794
SamerSacic <samer.sacic@gmail.com>
2021-10-20 21:11:16 +0200

    Initial Commit

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,31 @@
+import { useHistory } from "react-router-dom";
+
+import NewMeetupFrom from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetups-16409-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupFrom onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
